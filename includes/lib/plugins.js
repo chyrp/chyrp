40b38f0ff69382560ff2ca2b3f2858b8d6269318
Daniel Pimley <dan@pimley.net>
2013-07-14 16:09:28 +0100

    Making loader() non-intrusive
    Currently it's not possible for themes to customise the ajax loader.
    I've solved this problem by having the loader() function not append any
    elements to the DOM; instead it simply adds the class "ajax_loading" to
    the host element (post, comment) on which it has been called. With the
    power of CSS we can leverage the "ajax_loading" class to do almost
    anything we want to the host element in terms of graphical effects and
    displaying gadgets.
    
    Removing the DOM elements also sidesteps a problem I've noticed:
    loader() is not able to properly account for CSS box-sizing, meaning
    that it displays in the wrong position on screen when the host element
    is rendered with "box-sizing: border-box".

--- includes/lib/plugins.js
+++ includes/lib/plugins.js
@@ -216,44 +216,10 @@ $.fn.expand = function(){
 // "Loading..." overlay.
 $.fn.loader = function(remove) {
   if (remove) {
-    $(this).next().remove()
+	$(this).removeClass("ajax_loading");
     return this
   }
-
-  var offset = $(this).offset()
-  var loading_top = ($(this).outerHeight() / 2) - 11
-  var loading_left = ($(this).outerWidth() / 2) - 63
-
-  $(this).after("<div class=\"load_overlay\"><img src=\""+site_url+"/includes/close.png\" style=\"display: none\" class=\"close\" /><img src=\""+site_url+"/includes/loading.gif\" style=\"display: none\" class=\"loading\" /></div>")
-
-  $(".load_overlay .loading").css({
-    position: "absolute",
-    top: loading_top+"px",
-    left: loading_left+"px",
-    display: "inline"
-  })
-
-  $(".load_overlay .close").css({
-    position: "absolute",
-    top: "3px",
-    right: "3px",
-    color: "#fff",
-    cursor: "pointer",
-    display: "inline"
-  }).click(function(){ $(this).parent().remove() })
-
-  $(".load_overlay").css({
-    position: "absolute",
-    top: offset.top,
-    left: offset.left,
-    zIndex: 100,
-    width: $(this).outerWidth(),
-    height: $(this).outerHeight(),
-    background: ($.browser.msie) ? "transparent" : "transparent url('"+site_url+"/includes/trans.png')",
-    textAlign: "center",
-    filter: ($.browser.msie) ? "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled=true, sizingMethod=scale, src='"+site_url+"/includes/trans.png');" : ""
-  })
-
+  $(this).addClass("ajax_loading");
   return this
 }
 
