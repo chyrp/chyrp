948fffde06e8fcf522c26333560e824eb63631ff
Arian Xhezairi <arian@xhezairi.com>
2014-11-03 18:48:33 +0100

    Updated copyright note.

--- includes/class/TwigExtensions/Autoloader.php
+++ includes/class/TwigExtensions/Autoloader.php
@@ -1,19 +1,19 @@
 <?php
 
 /*
- * This file is part of Twig.
+ * This file is part of Chyrp.
  *
- * (c) 2009 Fabien Potencier
+ * (c) 2014 Arian Xhezairi
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 /**
- * Autoloads Twig Extensions classes.
+ * Autoloads Chyrp's Twig Extensions classes.
  *
- * @package    twig
- * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @package    chyrp
+ * @author     Arian Xhezairi <email@chyrp.net>
  */
 class Twig_Extensions_Autoloader
 {
